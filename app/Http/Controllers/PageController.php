0fbfe7ea924b650164c10b34de4c467fbc8011c8
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-07-08 12:51:42 +0500

    add all methods for routes

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -16,8 +16,38 @@ class PageController extends Controller
         return view('pages.index');
     }
 
+    public function contact()
+    {
+        return view('pages.contact');
+    }
+
+    public function gallery()
+    {
+        return view('pages.gallery');
+    }
+
+    public function blog()
+    {
+        return view('pages.blog');
+    }
+
     public function about()
     {
-        return view('pages.about.chairman-message.blade.php');
+        return view('pages.about.chairman-message');
+    }
+
+    public function about1()
+    {
+        return view('pages.about.organizational-strategy');
+    }
+
+    public function about2()
+    {
+        return view('pages.about.board-of-directors');
+    }
+
+    public function about3()
+    {
+        return view('pages.about.statutes-and-by-laws');
     }
 }
