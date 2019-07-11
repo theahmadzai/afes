d2810fd9bd4f3d7bfc7cde0a3df92337bb35b705
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-07-11 12:49:34 +0500

    add pages

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -33,20 +33,25 @@ class PageController extends Controller
 
     public function about()
     {
+        return redirect('about/chairman-message');
+    }
+
+    public function about_chairmanMessage()
+    {
         return view('pages.about.chairman-message');
     }
 
-    public function about1()
+    public function about_organizationalStrategy()
     {
         return view('pages.about.organizational-strategy');
     }
 
-    public function about2()
+    public function about_boardOfDirectors()
     {
         return view('pages.about.board-of-directors');
     }
 
-    public function about3()
+    public function about_statutesAndByLaws()
     {
         return view('pages.about.statutes-and-by-laws');
     }
