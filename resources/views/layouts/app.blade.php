dd4ca7b69e7d4c3ec5dc3ba1e2612fb5678ca83b
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-07-12 06:32:32 +0500

    add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -95,6 +95,43 @@
         <main>
             @yield('content')
         </main>
+
+        <footer class="py-5 bg-dark text-white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-5">
+                        <h5>Address</h5>
+                        <p class="mb-3 text-muted">
+                            House# 236, Behind District 4 Municipality Office 5 th Street, <br>
+                            40-Meter Road, Kulola Pushta Kabul, Afghanistan 1001, <br>
+                            Phone: +93 771 251 253
+                        </p>
+                    </div>
+                    <div class="col">
+                        <h5>Resources</h5>
+                        <ul class="list-unstyled text-small">
+                            <li><a class="text-muted" href="#">Business</a></li>
+                            <li><a class="text-muted" href="#">Education</a></li>
+                            <li><a class="text-muted" href="#">Government</a></li>
+                            <li><a class="text-muted" href="#">Gaming</a></li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h5>About</h5>
+                        <ul class="list-unstyled text-small">
+                            <li><a class="text-muted" href="#">Team</a></li>
+                            <li><a class="text-muted" href="#">Locations</a></li>
+                            <li><a class="text-muted" href="#">Privacy</a></li>
+                            <li><a class="text-muted" href="#">Terms</a></li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <img src="/img/origional.png" alt="AFES">
+                        {{-- <small class="d-block mb-3 text-muted">&copy; 2017-2019</small> --}}
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
 
     <script src="{{ asset('js/app.js') }}"></script>
