7016193481e7fed57855040901a1a9da65719c2e
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-07-17 20:39:06 +0500

    add breadcrumb

--- resources/views/pages/about/statutes-and-by-laws.blade.php
+++ resources/views/pages/about/statutes-and-by-laws.blade.php
@@ -3,7 +3,13 @@
 @section('title', 'Statutes And By-Laws')
 
 @section('content')
-<div class="container">
+<nav aria-label="breadcrumb container">
+    <ol class="breadcrumb align-items-center justify-content-center p-5">
+        <li class="breadcrumb-item"><a href="#">About Us</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Statutes And By-Laws</li>
+    </ol>
+</nav>
+<div class="container my-5">
     Statutes And By-Laws
 </div>
 @endsection
