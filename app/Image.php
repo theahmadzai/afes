42b6537961978d555bb5a546669d743ecfa0ad85
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-07-20 22:12:44 +0500

    add thumbnail functions

--- app/Image.php
+++ app/Image.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Storage;
 
 class Image extends Model
 {
@@ -12,5 +13,29 @@ class Image extends Model
     public function imageable()
     {
         return $this->morphTo();
+    }
+
+    public function getFilenameAttribute($value)
+    {
+        return Storage::url($value ?? 'default/image.png');
+    }
+
+    public function thumbnail()
+    {
+        if (!$this->getOriginal('image')) {
+            return $this->image;
+        }
+
+        return '/storage/thumbnails/' . basename($this->image);
+    }
+
+    public function createThumbnail($x = 300, $y = 150)
+    {
+        $image = InterventionImage::make(public_path($this->image))->fit($x, $y, function ($constraint) {
+            $constraint->upsize();
+            $constraint->aspectRatio();
+        })->encode();
+
+        Storage::put('public/thumbnails/' . basename($this->image), $image);
     }
 }
