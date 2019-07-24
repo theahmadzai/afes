8dece5b8a52872f3a0fb02c0aae555d662803580
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-07-25 04:11:09 +0500

    nova filters added

---
+++ app/Nova/Filters/PublishedPosts.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Nova\Filters;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Filters\Filter;
+
+class PublishedPosts extends Filter
+{
+    /**
+     * The filter's component.
+     *
+     * @var string
+     */
+    public $component = 'select-filter';
+
+    /**
+     * Apply the filter to the given query.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  mixed  $value
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(Request $request, $query, $value)
+    {
+        return $query->where('is_published', $value == 'only-published');
+    }
+
+    /**
+     * Get the filter's available options.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function options(Request $request)
+    {
+        return [
+            'Only Published Posts' => 'only-published',
+            'Only Unpublished Posts' => 'only-unpublished'
+        ];
+    }
+}
