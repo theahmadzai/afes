793cc6f30a3724cda45277c17d10d4971421c6ea
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-07-25 04:08:51 +0500

    Tag factory added

---
+++ database/factories/TagFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Tag;
+use Faker\Generator as Faker;
+
+$factory->define(Tag::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'slug' => $faker->slug
+    ];
+});
