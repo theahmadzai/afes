24cb3b40705d4bfe066444ae6a084840a4a1cea9
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-07-25 21:27:14 +0500

    blog post page

---
+++ resources/views/pages/post.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('title', 'Post')
+
+@section('content')
+<div class="container my-5">
+
+    <div class="row mb-4">
+        <div class="col-3">
+            <img src="{{ asset('storage/' . $post->image->filename) }}" alt="{{ $post->image->caption }}" class="img-fluid">
+        </div>
+
+        <div class="col">
+            <h3>{{ $post->title }}</h3>
+
+            <p class="text-muted">{{ $post->published_at->toFormattedDateString() }}</p>
+
+            <p>Tags: <b>{{ $post->tags->implode('name', ', ') }}</b></p>
+        </div>
+    </div>
+
+    <p class="lead">{{ $post->body }}</p>
+
+    <b>Posted by, {{ $post->user->name }}</b>
+
+</div>
+@endsection
