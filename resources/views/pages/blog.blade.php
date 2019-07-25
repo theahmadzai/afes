07f7c68cf2b6d4b787d5b65f16027acf24be7431
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-07-25 21:27:08 +0500

    blog page

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -3,7 +3,27 @@
 @section('title', 'Blog')
 
 @section('content')
-<div class="container">
-    Blog
+<div class="container my-5">
+
+    @foreach($posts as $post)
+
+        <div class="row border rounded no-gutters overflow-hidden shadow-sm my-3">
+            <div class="col-auto d-none d-lg-block">
+                <img class="bd-placeholder-img" width="250" height="250" src="{{ asset('storage/' . $post->image->filename) }}"></img>
+            </div>
+
+            <div class="col p-4 d-flex flex-column position-static">
+                <strong class="d-inline-block mb-2 text-primary">{{ $post->tags->first()->name }}</strong>
+                <h3 class="mb-0">{{ $post->title }}</h3>
+                <div class="mb-1 text-muted">{{ $post->published_at->diffForHumans() }}</div>
+                <p class="card-text mb-auto">{{ str_limit($post->body, 200) }}</p>
+                <a href="{{ url('blog/' . $post->slug) }}" class="stretched-link">Continue reading</a>
+            </div>
+        </div>
+
+    @endforeach
+
+    {{ $posts->links() }}
+
 </div>
 @endsection
