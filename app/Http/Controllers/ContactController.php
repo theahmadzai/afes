6280a1de2fb3b992a13bd123017adaebbbf9e772
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-07-26 23:43:08 +0500

    add method mail

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
+use Mail;
+use App\Http\Requests\Message as MessageRequest;
+use App\Mail\Contact;
+use App\Message;
 
 class ContactController extends Controller
 {
     public function index()
     {
         return View::make('pages.contact');
+    }
+
+    public function mail(MessageRequest $request)
+    {
+        Mail::send(new Contact(Message::create($request->validated())));
+
+        return back();
     }
 }
