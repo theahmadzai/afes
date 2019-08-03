1a305325d6ad4e9fa2ea1a1a281ff5e7573797f7
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-08-03 21:25:54 +0500

    add post obsrever

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Post;
+use App\Observers\PostObserver;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -25,5 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        Post::observe(PostObserver::class);
     }
 }
