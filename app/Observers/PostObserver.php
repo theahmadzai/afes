8a62da5cf07c8a33e875f015aff353d6a7abf901
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-08-03 21:25:44 +0500

    slugify

--- app/Observers/PostObserver.php
+++ app/Observers/PostObserver.php
@@ -4,11 +4,13 @@ namespace App\Observers;
 
 use App\Post;
 use Illuminate\Support\Facades\Auth;
+use Str;
 
 class PostObserver
 {
     public function creating(Post $post)
     {
+        $post->slug = Str::slug($post->title);
         $post->user_id = Auth::id();
     }
 }
