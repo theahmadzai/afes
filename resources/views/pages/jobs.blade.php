03ddd0749077b3b112011077dbbadd664c57a643
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-08-04 18:22:14 +0500

    job view

---
+++ resources/views/pages/jobs.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('title', 'Jobs')
+
+@section('content')
+<div class="container my-5">
+
+    @foreach($jobs as $job)
+
+        <div class="row border rounded no-gutters overflow-hidden shadow-sm my-3 p-3">
+            <h3 class="mb-0">{{ $job->title }}</h3>
+            <div class="mb-1 text-muted">{{ $job->posted_at->diffForHumans() }}</div>
+            <div class="mb-1 text-muted">{{ $job->closing_at->diffForHumans() }}</div>
+            <p class="card-text mb-auto">{{ str_limit($job->job_profile, 200) }}</p>
+        </div>
+
+    @endforeach
+
+    {{ $jobs->links() }}
+
+</div>
+@endsection
