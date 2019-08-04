604e21f20a2759ef91231b2354311935619c17e1
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-08-04 14:38:05 +0500

    fix

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -11,7 +11,7 @@ $factory->define(Post::class, function (Faker $faker) {
         'title' => $faker->sentence(6),
         'slug' => $faker->slug,
         'body' => $faker->sentence(200),
-        'is_published' => $faker->randomElement([true, false]),
+        'is_published' => $faker->boolean,
         'published_at' => now()->subDays(random_int(1, 180)),
         'user_id' => $faker->randomElement(User::pluck('id')->toArray()),
     ];
