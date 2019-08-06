92c91c31d2814df755bfd22c64eda1d80872d7e7
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-08-06 20:45:10 +0500

    rename to thumbnail

---
+++ app/Traits/Thumbnail.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Traits;
+
+use Intervention\Image\Facades\Image as InterventionImage;
+use App\File;
+use Storage;
+
+trait Thumbnail
+{
+    public static $imageables = [
+        \App\Image::class,
+        \App\Slide::class,
+        \App\Post::class,
+    ];
+
+    public static function createThumbnail(File $file, $x = 300, $y = 150)
+    {
+        if (in_array($file->fileable_type, self::$imageables)) {
+            $image = InterventionImage::make(public_path(Storage::url($file->filename)))->fit($x, $y, function ($constraint) {
+                $constraint->upsize();
+                $constraint->aspectRatio();
+            })->encode();
+
+            Storage::disk('public')->put('thumbnails/' . basename($file->filename), $image);
+        }
+    }
+}
