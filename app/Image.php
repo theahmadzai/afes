7cf201cb17935f5565f640ac9ab8bf54c87dec5e
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-08-06 12:30:01 +0500

    rename

--- app/Image.php
+++ app/Image.php
@@ -3,34 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Storage;
 
 class Image extends Model
 {
-    use SoftDeletes;
-
-    public function imageable()
+    public function category()
     {
-        return $this->morphTo();
+        return $this->belongsTo(Category::class);
     }
 
-    public function thumbnail()
+    public function file()
     {
-        if (!$this->getOriginal('image')) {
-            return $this->image;
-        }
-
-        return '/storage/thumbnails/' . basename($this->image);
-    }
-
-    public function createThumbnail($x = 300, $y = 150)
-    {
-        $image = InterventionImage::make(public_path($this->image))->fit($x, $y, function ($constraint) {
-            $constraint->upsize();
-            $constraint->aspectRatio();
-        })->encode();
-
-        Storage::put('public/thumbnails/' . basename($this->image), $image);
+        return $this->morphOne(File::class, 'fileable');
     }
 }
