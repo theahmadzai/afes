ffa29e295efb839e4e1130bd6ec0ce06007d1a44
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-08-16 00:06:30 +0500

    subscriber database setup

---
+++ database/factories/SubscriberFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Subscriber;
+use Faker\Generator as Faker;
+
+$factory->define(Subscriber::class, function (Faker $faker) {
+    return [
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'email' => $faker->email,
+    ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             MessagesTableSeeder::class,
             JobsTableSeeder::class,
             ApplicationsTableSeeder::class,
+            SubscribersTableSeeder::class,
         ]);
     }
 }
