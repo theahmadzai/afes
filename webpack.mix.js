19aacb0d0e897d65279e9d496a7785f4775d9f7e
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-09-02 19:15:01 +0500

    updated

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/vendor.scss', 'public/css/build')
+    .sass('resources/sass/app.scss', 'public/css/build')
+    .styles([
+        'public/css/build/vendor.css',
+        'public/css/build/app.css'
+    ], 'public/css/app.css');
