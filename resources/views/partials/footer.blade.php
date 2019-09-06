a214796ff41527c344735cd146fefaeb56ce4258
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-09-07 02:28:40 +0500

    update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,33 +1,33 @@
-<footer class="container bg-dark text-white app-footer">
+<footer class="container app-footer">
 
     <div class="row py-3">
 
-        <div class="col-6 p-5">
-            <h6>Address</h6>
-            <p class="mb-3 text-muted">
+        <div class="col-md-6 p-5">
+            <h6 class="mb-3">Address</h6>
+            <p>
                 House# 236, Behind District 4 Municipality Office 5 th Street, <br>
                 40-Meter Road, Kulola Pushta Kabul, Afghanistan 1001, <br>
                 Phone: +93 771 251 253
             </p>
         </div>
 
-        <div class="col-3 p-5">
-            <h6>Resources</h6>
-            <ul class="list-unstyled text-small">
-                <li><a class="text-muted" href="#">Business</a></li>
-                <li><a class="text-muted" href="#">Education</a></li>
-                <li><a class="text-muted" href="#">Government</a></li>
-                <li><a class="text-muted" href="#">Gaming</a></li>
+        <div class="col-sm-3 p-5">
+            <h6 class="mb-3">Resources</h6>
+            <ul class="list-unstyled">
+                <li><a href="#">Business</a></li>
+                <li><a href="#">Education</a></li>
+                <li><a href="#">Government</a></li>
+                <li><a href="#">Gaming</a></li>
             </ul>
         </div>
 
-        <div class="col-3 p-5">
-            <h6>About</h6>
-            <ul class="list-unstyled text-small">
-                <li><a class="text-muted" href="#">Team</a></li>
-                <li><a class="text-muted" href="#">Locations</a></li>
-                <li><a class="text-muted" href="#">Privacy</a></li>
-                <li><a class="text-muted" href="#">Terms</a></li>
+        <div class="col-sm-3 p-5">
+            <h6 class="mb-3">About</h6>
+            <ul class="list-unstyled">
+                <li><a href="#">Team</a></li>
+                <li><a href="#">Locations</a></li>
+                <li><a href="#">Privacy</a></li>
+                <li><a href="#">Terms</a></li>
             </ul>
         </div>
 
