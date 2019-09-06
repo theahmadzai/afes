3815527b90c38bd9ba5cb71110b2125999e27461
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-09-06 22:10:45 +0500

    updated

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,33 +6,42 @@
 
 @include('partials.carousel')
 
+@component('partials.head')
+    Afghan Evaluation Society
+@endcomponent
+
 <div class="container">
+
     <div class="my-4">
-        <h1>Afghan Evaluation Society</h1>
         <p>Afghan Evaluation Society (AfES) is a non-profit and Afghan-led organization registered with the Afghan Ministry of Economy as an NGO; that aims to strengthen evaluations for Sustainable Development in Afghanistan.</p>
         <p>AfES intends to work with the government, civil society and development agencies to strengthen evaluations and promote the Sustainable Development agenda in Afghanistan. Evaluation is being increasingly important due to the fact that it needs to support the new Sustainable Development Goals (SDGs).</p>
         <p>AfES is established by a number of professionals in the field of Evaluation whose primary objective is to promote the theory, practice and use of evaluation. And, to strengthen individual and institutional capacities for Evaluation to support evidence-based policy making and improved programming in Afghanistan.</p>
     </div>
 
-    <div class="row">
-        <div class="col-lg-4 p-3">
-            <h2 class="display-5 font-weight-bold">Vision</h2>
-            <p class="lead">Strengthened Evaluations for Sustainable Development in Afghanistan</p>
+    <div class="row p-3 text-center app-aim">
+
+        <div class="col-md-4 p-3">
+            <h2 class="font-weight-bold mb-2">Vision</h2>
+            <p>Strengthened Evaluations for Sustainable Development in Afghanistan</p>
         </div>
-        <div class="col-lg-4 p-3">
-            <h2 class="display-5 font-weight-bold">Mission</h2>
-            <p class="lead">To establish a leading platform for evaluation through capacity building, advocacy and networking, in order to support evidence-based decision making for sustainable development in Afghanistan.</p>
+
+        <div class="col-md-4 p-3">
+            <h2 class="font-weight-bold mb-2">Mission</h2>
+            <p>To establish a leading platform for evaluation through capacity building, advocacy and networking, in order to support evidence-based decision making for sustainable development in Afghanistan.</p>
         </div>
-        <div class="col-lg-4 p-3">
-            <h2 class="display-5 font-weight-bold">Values</h2>
-            <ul class="list-unstyled lead">
+
+        <div class="col-md-4 p-3">
+            <h2 class="font-weight-bold mb-2">Values</h2>
+            <ul class="list-unstyled">
                 <li>Integrity</li>
                 <li>Equity</li>
                 <li>Commitment</li>
                 <li>Quality</li>
             </ul>
         </div>
+
     </div>
+
 </div>
 
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,15 +1,18 @@
-<footer class="container bg-dark text-white">
+<footer class="container bg-dark text-white app-footer">
+
     <div class="row py-3">
-        <div class="col-6">
-            <h5>Address</h5>
+
+        <div class="col-6 p-5">
+            <h6>Address</h6>
             <p class="mb-3 text-muted">
                 House# 236, Behind District 4 Municipality Office 5 th Street, <br>
                 40-Meter Road, Kulola Pushta Kabul, Afghanistan 1001, <br>
                 Phone: +93 771 251 253
             </p>
         </div>
-        <div class="col">
-            <h5>Resources</h5>
+
+        <div class="col-3 p-5">
+            <h6>Resources</h6>
             <ul class="list-unstyled text-small">
                 <li><a class="text-muted" href="#">Business</a></li>
                 <li><a class="text-muted" href="#">Education</a></li>
@@ -17,8 +20,9 @@
                 <li><a class="text-muted" href="#">Gaming</a></li>
             </ul>
         </div>
-        <div class="col">
-            <h5>About</h5>
+
+        <div class="col-3 p-5">
+            <h6>About</h6>
             <ul class="list-unstyled text-small">
                 <li><a class="text-muted" href="#">Team</a></li>
                 <li><a class="text-muted" href="#">Locations</a></li>
@@ -26,5 +30,7 @@
                 <li><a class="text-muted" href="#">Terms</a></li>
             </ul>
         </div>
+
     </div>
+
 </footer>
