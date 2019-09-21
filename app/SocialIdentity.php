39bc69b50d1f9aba7373ad847940db96e969327d
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-09-22 04:45:55 +0500

    social identity model

---
+++ app/SocialIdentity.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SocialIdentity extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'provider_id',
+        'provider_name',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
