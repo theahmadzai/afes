56b1ba3a3be85bdb712c148f7c4d81ff85423276
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-09-21 18:46:22 +0500

    breadcrumb for about page

---
+++ resources/views/partials/breadcrumb.blade.php
@@ -0,0 +1,6 @@
+<nav aria-label="Breadcrumb">
+    <ol class="breadcrumb align-items-center justify-content-center p-4 m-0">
+        <li class="breadcrumb-item"><a href="{{ url('about') }}">About Us</a></li>
+        <li class="breadcrumb-item active" aria-current="page">{{ $slot }}</li>
+    </ol>
+</nav>
