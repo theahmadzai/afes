c982ab56bf7b2e008e521e19685202f8412fbea9
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-09-22 04:45:20 +0500

    add check for null image

--- app/Traits/Imagify.php
+++ app/Traits/Imagify.php
@@ -7,16 +7,28 @@ trait Imagify
 {
     public function getLocationAttribute()
     {
+        if(!$this->file) {
+            return 'thumbnails/default.png';
+        }
+
         return 'thumbnails/' . basename($this->file->filename);
     }
 
     public function getThumbnailAttribute()
     {
+        if(!$this->file) {
+            return 'thumbnails/default.png';
+        }
+
         return Storage::disk('public')->url('thumbnails/' . basename($this->file->filename));
     }
 
     public function getImageAttribute()
     {
+        if(!$this->file) {
+            return 'files/default.png';
+        }
+
         return Storage::disk('public')->url($this->file->filename);
     }
 }
