eadbe11bee8244dba5dcbf94209f8766218b8687
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-09-22 04:40:37 +0500

    add font awesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,8 @@
 
     <link rel="icon" href="/img/logo.png">
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito">
-    <link href="https://fonts.googleapis.com/css?family=Nunito|Lato|Open+Sans|Roboto:400,400i,500,500i,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito|Lato|Open+Sans|Roboto:400,400i,500,500i,700&display=swap">
 
     @stack('styles')
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
