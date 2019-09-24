aac2c7ef53b6d96848b2e8c6aa793b31d4e31e9d
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-09-24 17:08:36 +0500

    fix

--- resources/views/flash/success.blade.php
+++ resources/views/flash/success.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Success')
+@section('title', $header)
 
 @section('content')
 <div class="container my-5">
@@ -8,7 +8,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ $header }}</div>
+                <div class="card-header">{{ $head }}</div>
 
                 <div class="card-body">
                     {{ $body }}
