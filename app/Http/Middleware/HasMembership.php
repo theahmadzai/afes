7567d9b8c79340165717446ce9105264a5573d5e
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-09-24 10:22:53 +0500

    add member middleware

---
+++ app/Http/Middleware/HasMembership.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class HasMembership
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->user() && (
+            !$request->user()->subscribed('student') ||
+            !$request->user()->subscribed('professional') ||
+            !$request->user()->subscribed('institutional') )) {
+            return redirect('membership');
+        }
+
+        return $next($request);
+    }
+}
