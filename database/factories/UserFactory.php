cf7a26c60323953c63a6ba353b28546512676d71
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-09-24 16:17:56 +0500

    add username in factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Hash;
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => 'Tester Bot',
+        'username'=> 'tester',
         'email' => 'tester@test.com',
         'email_verified_at' => now(),
         'password' => Hash::make('tester'),
