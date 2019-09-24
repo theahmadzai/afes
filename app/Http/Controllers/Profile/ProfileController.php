fdb3356a195128330872864b5940d18296be95c2
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-09-24 16:18:53 +0500

    profile

---
+++ app/Http/Controllers/Profile/ProfileController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Profile;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\View;
+use App\User;
+
+class ProfileController extends Controller
+{
+    public function index(User $user)
+    {
+        return View::make('profile.index', compact('user'));
+    }
+}
