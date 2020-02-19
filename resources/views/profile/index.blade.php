f9697b73d2a67aaa7d9b834d9c2681ffd15788b3
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-02-19 19:05:32 +0500

    Design updated

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -14,101 +14,51 @@
             <div class="col-md-4">
                 <img src="{{ $user->gravatar }}" alt="{{ $user->name }}" class="w-100 shadow img-thumbnail mb-2">
             </div>
-            <div class="col-md-4">
-                <p><b>Name</b></p>
-                <p><b>Username</b></p>
-                <p><b>Email Address</b></p>
-                <p><b>Gender</b></p>
-                <p><b>Email Address</b></p>
-                <p><b>Email Address</b></p>
-            </div>
-            <div class="col-md-4">
-                <p>{{ $user->name }}</p>
-                <p>{{ $user->username }}</p>
-                <p>{{ $user->email }}</p>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-4">
-                <p>Name</p>
-            </div>
-            <div class="col-md-4">
-                <p>{{ $user->name }}</p>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-4">
-                <p>Username</p>
-            </div>
-            <div class="col-md-4">
-                <p>{{ $user->username }}</p>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-4">
-                <p>Email Address</p>
-            </div>
-            <div class="col-md-4">
-                <p>{{ $user->email }}</p>
+            <div class="col-md-8 table-responsive">
+                <table class="table table-striped">
+                    <tbody>
+                        <tr>
+                            <th scope="row">Name</th>
+                            <td>{{ $user->name }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Username</th>
+                            <td>{{ $user->username }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Email Address</th>
+                            <td>{{ $user->email_address }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Gender</th>
+                            <td>{{ $user->gender }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Birth Date</th>
+                            <td>{{ $user->birth_date }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">City</th>
+                            <td>{{ $user->city }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Phone</th>
+                            <td>{{ $user->phone }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Address</th>
+                            <td>{{ $user->address }}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
 
-        <div class="row">
-            <div class="col-md-4">
-                <p>Gender</p>
-            </div>
-            <div class="col-md-4">
-                <p>{{ ucfirst($user->gender) }}</p>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-4">
-                <p>Birth Date</p>
-            </div>
-            <div class="col-md-4">
-                <p>{{ $user->birth_date }}</p>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-4">
-                <p>City</p>
-            </div>
-            <div class="col-md-4">
-                <p>{{ $user->city }}</p>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-4">
-                <p>Address</p>
-            </div>
-            <div class="col-md-4">
-                <p>{{ $user->address }}</p>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-4">
-                <p>Phone</p>
-            </div>
-            <div class="col-md-4">
-                <p>{{ $user->phone }}</p>
-            </div>
-        </div>
-
-        <table class="table table-striped">
+        <ul class="list-group list-group-flush">
         @foreach ($user->posts as $post)
-            <tr>
-                <td>{{ $post->title }}</td>
-                <td>{{ str_limit($post->body, 150) }}</td>
-            </tr>
+            <a href="{{ url('blog/' . $post->slug) }}"><li class="list-group-item">{{ $post->title }}</li></a>
         @endforeach
-        </table>
+        </ul>
 
     </div>
 
