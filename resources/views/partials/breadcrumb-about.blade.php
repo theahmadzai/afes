9a971d18c47ed967065315ce19606978bf6270bf
theahmadzai <theahmadzai@hotmail.com>
2020-02-20 19:39:45 +0500

    breadcrumb for about

---
+++ resources/views/partials/breadcrumb-about.blade.php
@@ -0,0 +1,7 @@
+<nav aria-label="Breadcrumb">
+    <ol class="breadcrumb align-items-center justify-content-center p-4 m-0">
+        <li class="breadcrumb-item"><a href="{{ url('about') }}">About Us</a></li>
+        <li class="breadcrumb-item"><a href="{{ url('about/statutes-and-by-laws') }}">Statutes And By-Laws</a></li>
+        <li class="breadcrumb-item active" aria-current="page">{{ $slot }}</li>
+    </ol>
+</nav>
