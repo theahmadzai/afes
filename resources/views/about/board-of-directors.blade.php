cdc925fd45a53dc98d6e51d86c4c7e8c81677e20
theahmadzai <theahmadzai@hotmail.com>
2020-03-10 22:51:28 +0500

    add section list + refactor

--- resources/views/about/board-of-directors.blade.php
+++ resources/views/about/board-of-directors.blade.php
@@ -12,58 +12,76 @@
         Board Of Directors
     @endcomponent
 
-    <div class="container text-justify py-3">
+    <div class="container-fluid">
+
         <div class="row">
-            <div class="col">
-                <div class="image-wrapper float-left w-25 pr-2">
-                    <img class="img-thumbnail img-responsive" src="/img/bod/Samandar-Mahmodi.jpg" alt="Chairman AFES">
+
+            <div class="col-md-9 p-4 text-justify">
+
+                <div class="row">
+                    <div class="col">
+                        <div class="image-wrapper float-left w-25 pr-2">
+                            <img class="img-thumbnail img-responsive" src="/img/bod/Samandar-Mahmodi.jpg" alt="Chairman AFES">
+                        </div>
+                        <p><b>Mr. Samandar Mahmodi</b> has over ten (10) years of professional experience in social research, surveys, monitoring and evaluation, qualitative and quantitative data collection and analysis; designing, developing and managing M&E systems. Mr Mahmodi has worked with a broad range of programs and projects in the areas of civil society empowerment, anti-corruption and good governance, private sector investment and capacity building.</p>
+                        <p>He has also led the National M&E Stakeholders’ Assessment and contributed to the developed of National RBME Policy Framework for the Office of the President, and has led, managed and worked as a team member in over 30 national research, surveys and evaluations in Afghanistan.</p>
+                        <p>Mr Mahmodi has an Associate degree in Computer Sciences, a Bachelor degree in Business Administration and an International Certificate in International Program for Development Evaluation Training (IPDET) from Carleton University, Ottawa, Canada. Mr Mahmodi has also attended professional trainings in anti-corruption from Malaysian Anti-Corruption Academy, and Central European University (CEU) in Hungary. He is an advocator, fan and lobbyist of evaluation for sustainable development in Afghanistan and the region.</p>
+                    </div>
+                    <div class="col">
+                        <div class="image-wrapper float-left w-25 pr-2">
+                            <img class="img-thumbnail img-responsive" src="/img/bod/Imad-Ahmad-Haroon.jpg" alt="Chairman AFES">
+                        </div>
+                        <p><b>Mr. Imad Ahmad Haroon</b> is a Development, Monitoring and Evaluation expert. He has over 9 years of experience in the field of monitoring, evaluation and development in Afghanistan. He has extensive experience in private sector development, government and the civil society with various donors, including; USAID, DFID, CIDA, GIZ and the World Bank.</p>
+                        <p>Mr. Haroon is currently working with GIZ Afghanistan as Head of Monitoring and Evaluation Unit. Previously, he worked as Monitoring and Evaluation Director with USAID/Chemonics; Results-based Management Advisor with GIZ/IDEA, and Monitoring and Evaluation Manager with Harakat-AICP; Research, Monitoring and Evaluation Manager with International Rescue Committee (IRC); and Data Management Officer with Coordination of Afghan Relief (CoAR).</p>
+                        <p>Mr. Haroon also developed M&E tools for the Policy Coordination Unit of the Afghan President and developed the M&E system for the Afghan Ministry of Energy and Water (MEW). As part of a team he contributed in the M&E assessment of Afghan Ministry of Finance (MoF). Mr. Haroon is a founding board member of Afghan Evaluation Society (AfES) since 2015, and an active member of EvalYouth, and is also a professional member of European Evaluation Society (EES) and Canadian Evaluation Society (CES).</p>
+                    </div>
                 </div>
-                <p><b>Mr. Samandar Mahmodi</b> has over ten (10) years of professional experience in social research, surveys, monitoring and evaluation, qualitative and quantitative data collection and analysis; designing, developing and managing M&E systems. Mr Mahmodi has worked with a broad range of programs and projects in the areas of civil society empowerment, anti-corruption and good governance, private sector investment and capacity building.</p>
-                <p>He has also led the National M&E Stakeholders’ Assessment and contributed to the developed of National RBME Policy Framework for the Office of the President, and has led, managed and worked as a team member in over 30 national research, surveys and evaluations in Afghanistan.</p>
-                <p>Mr Mahmodi has an Associate degree in Computer Sciences, a Bachelor degree in Business Administration and an International Certificate in International Program for Development Evaluation Training (IPDET) from Carleton University, Ottawa, Canada. Mr Mahmodi has also attended professional trainings in anti-corruption from Malaysian Anti-Corruption Academy, and Central European University (CEU) in Hungary. He is an advocator, fan and lobbyist of evaluation for sustainable development in Afghanistan and the region.</p>
-            </div>
-            <div class="col">
-                <div class="image-wrapper float-left w-25 pr-2">
-                    <img class="img-thumbnail img-responsive" src="/img/bod/Imad-Ahmad-Haroon.jpg" alt="Chairman AFES">
+
+                <hr>
+
+                <div class="row pt-4">
+                    <div class="col">
+                        <div class="image-wrapper float-left w-25 pr-2">
+                            <img class="img-thumbnail img-responsive" src="/img/bod/Qudratullah-Jahid.jpg" alt="Chairman AFES">
+                        </div>
+                        <p><b>Mr. Qudratullah Jahid</b> is the Secretary General and founding board member of Afghan Evaluation Society (AfES). Mr Jahid is an M&E professional and sustainable development enthusiast. He has a bachelor’s degree and four international certificates in management and M&E and has more than nine years of work experience with Afghan government and international organizations under USAID-funded programs and projects aimed at improving Afghans’ lives.</p>
+                        <p>Mr. Jahid is Secretary of the EvalYouth’s Task Force 2 that runs a global mentoring program. He is member of the International Development Evaluation Association (IDEAS), American Evaluation Association (AEA), Community of Evaluators (CoE) South Asia, European Evaluation Society and EvalSDGs network. Mr. Jahid is currently pursuing his master’s in International Development and Social Change focused on M&E at Clark University in Massachusetts, United States.</p>
+                    </div>
+                    <div class="col">
+                        <div class="image-wrapper float-left w-25 pr-2">
+                            <img class="img-thumbnail img-responsive" src="/img/bod/Tooryalay-Nasery.jpg" alt="Chairman AFES">
+                        </div>
+                        <p><b>Mr. Tooryalay Nasery</b> has a Master Degree in Management of Development from University of Applied Sciences Van Hall Larenstein, Wageningen, in The Netherlands; and has B.Sc. Agriculture from Kabul University. Mr. Nasery has over ten (10) years of experience in the fields of Monitoring and Evaluation, Research, Project Management, Environmental and Social Management and Reporting with different international organizations.</p>
+                        <p>Mr. Nasery is a founding board member of Afghan Evaluation Society (AfES) since 2015, and is currently working as M&E Advisor with USAID funded project of Afghanistan Workforce Development (AWDP) in Kabul. Furthermore, he has attended various M&E trainings abroad; and has also provided various trainings in the field of M&E, Statistical Data Analysis, Research and Project Management in Afghanistan.</p>
+                    </div>
                 </div>
-                <p><b>Mr. Imad Ahmad Haroon</b> is a Development, Monitoring and Evaluation expert. He has over 9 years of experience in the field of monitoring, evaluation and development in Afghanistan. He has extensive experience in private sector development, government and the civil society with various donors, including; USAID, DFID, CIDA, GIZ and the World Bank.</p>
-                <p>Mr. Haroon is currently working with GIZ Afghanistan as Head of Monitoring and Evaluation Unit. Previously, he worked as Monitoring and Evaluation Director with USAID/Chemonics; Results-based Management Advisor with GIZ/IDEA, and Monitoring and Evaluation Manager with Harakat-AICP; Research, Monitoring and Evaluation Manager with International Rescue Committee (IRC); and Data Management Officer with Coordination of Afghan Relief (CoAR).</p>
-                <p>Mr. Haroon also developed M&E tools for the Policy Coordination Unit of the Afghan President and developed the M&E system for the Afghan Ministry of Energy and Water (MEW). As part of a team he contributed in the M&E assessment of Afghan Ministry of Finance (MoF). Mr. Haroon is a founding board member of Afghan Evaluation Society (AfES) since 2015, and an active member of EvalYouth, and is also a professional member of European Evaluation Society (EES) and Canadian Evaluation Society (CES).</p>
+
+                <hr>
+
+                <div class="row pt-4">
+                    <div class="col">
+                        <div class="image-wrapper float-left w-25 pr-2">
+                            <img class="img-thumbnail img-responsive" src="/img/profile-placeholder.jpg" alt="Chairman AFES">
+                        </div>
+                        <p><b>Mr. Raoof Modaqiq</b> is a development practitioner with more than 12 years of professional working experience in the fields of monitoring, evaluation, research and private sector development both on operational and policy levels. He has worked with different national and international development organizations such as GIZ, GFA Consultancy Group, DoD-PSD, USAID-MSI, and FBAO.</p>
+                        <p>Currently, Mr. Modaqiq is a board member of the Afghan Evaluation Society and Senior Monitoring and Evaluation Advisor in Deutsche Gesellschaft für Internationale Zusammenarbeit (GIZ) GmbH. On behalf of the German Development Cooperation in Afghanistan, Mr. Modaqiq supports the Afghan Government in Institutionalizing M&E to measure the development efforts.</p>
+                        <p>Prior to this position, between 2006 and 2012, he was working as a program coordinator for private sector development, more precisely on business and value chain development with the German Development Consultancy Group GFA in Kabul and in the Northern provinces of Afghanistan.</p>
+                        <p>Mr. Modaqiq is a graduate of development studies with specialized courses in leadership, project management and organizational development.</p>
+                    </div>
+                    <div class="col">
+                        <div class="image-wrapper float-left w-25 pr-2">
+                            <img class="img-thumbnail img-responsive" src="/img/bod/Mansoor-Yosufzai.jpg" alt="Chairman AFES">
+                        </div>
+                        <p><b>Mr. Mansoor Yosufzai</b> has a bachelor’s degree in Business Administration with over five years of experience in the fields of Monitoring and Evaluation, Research, Project Management and Reporting with various international organizations in Afghanistan and Sweden.Mr. Yosufzai is a board member of Afghan Evaluation Society (AfES), and is currently working with Reuters News Bureau in Stockholm, Sweden.</p>
+                    </div>
+                </div>
+
             </div>
+
+            @include('partials.sectionlist')
+
         </div>
-        <div class="row">
-            <div class="col">
-                <div class="image-wrapper float-left w-25 pr-2">
-                    <img class="img-thumbnail img-responsive" src="/img/bod/Qudratullah-Jahid.jpg" alt="Chairman AFES">
-                </div>
-                <p><b>Mr. Qudratullah Jahid</b> is the Secretary General and founding board member of Afghan Evaluation Society (AfES). Mr Jahid is an M&E professional and sustainable development enthusiast. He has a bachelor’s degree and four international certificates in management and M&E and has more than nine years of work experience with Afghan government and international organizations under USAID-funded programs and projects aimed at improving Afghans’ lives.</p>
-                <p>Mr. Jahid is Secretary of the EvalYouth’s Task Force 2 that runs a global mentoring program. He is member of the International Development Evaluation Association (IDEAS), American Evaluation Association (AEA), Community of Evaluators (CoE) South Asia, European Evaluation Society and EvalSDGs network. Mr. Jahid is currently pursuing his master’s in International Development and Social Change focused on M&E at Clark University in Massachusetts, United States.</p>
-            </div>
-            <div class="col">
-                <div class="image-wrapper float-left w-25 pr-2">
-                    <img class="img-thumbnail img-responsive" src="/img/bod/Tooryalay-Nasery.jpg" alt="Chairman AFES">
-                </div>
-                <p><b>Mr. Tooryalay Nasery</b> has a Master Degree in Management of Development from University of Applied Sciences Van Hall Larenstein, Wageningen, in The Netherlands; and has B.Sc. Agriculture from Kabul University. Mr. Nasery has over ten (10) years of experience in the fields of Monitoring and Evaluation, Research, Project Management, Environmental and Social Management and Reporting with different international organizations.</p>
-                <p>Mr. Nasery is a founding board member of Afghan Evaluation Society (AfES) since 2015, and is currently working as M&E Advisor with USAID funded project of Afghanistan Workforce Development (AWDP) in Kabul. Furthermore, he has attended various M&E trainings abroad; and has also provided various trainings in the field of M&E, Statistical Data Analysis, Research and Project Management in Afghanistan.</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <div class="image-wrapper float-left w-25 pr-2">
-                    <img class="img-thumbnail img-responsive" src="/img/profile-placeholder.jpg" alt="Chairman AFES">
-                </div>
-                <p><b>Mr. Raoof Modaqiq</b> is a development practitioner with more than 12 years of professional working experience in the fields of monitoring, evaluation, research and private sector development both on operational and policy levels. He has worked with different national and international development organizations such as GIZ, GFA Consultancy Group, DoD-PSD, USAID-MSI, and FBAO.</p>
-                <p>Currently, Mr. Modaqiq is a board member of the Afghan Evaluation Society and Senior Monitoring and Evaluation Advisor in Deutsche Gesellschaft für Internationale Zusammenarbeit (GIZ) GmbH. On behalf of the German Development Cooperation in Afghanistan, Mr. Modaqiq supports the Afghan Government in Institutionalizing M&E to measure the development efforts.</p>
-                <p>Prior to this position, between 2006 and 2012, he was working as a program coordinator for private sector development, more precisely on business and value chain development with the German Development Consultancy Group GFA in Kabul and in the Northern provinces of Afghanistan.</p>
-                <p>Mr. Modaqiq is a graduate of development studies with specialized courses in leadership, project management and organizational development.</p>
-            </div>
-            <div class="col">
-                <div class="image-wrapper float-left w-25 pr-2">
-                    <img class="img-thumbnail img-responsive" src="/img/bod/Mansoor-Yosufzai.jpg" alt="Chairman AFES">
-                </div>
-                <p><b>Mr. Mansoor Yosufzai</b> has a bachelor’s degree in Business Administration with over five years of experience in the fields of Monitoring and Evaluation, Research, Project Management and Reporting with various international organizations in Afghanistan and Sweden.Mr. Yosufzai is a board member of Afghan Evaluation Society (AfES), and is currently working with Reuters News Bureau in Stockholm, Sweden.</p>
-            </div>
-        </div>
+
     </div>
 
 @endsection
--- resources/views/about/chairman-message.blade.php
+++ resources/views/about/chairman-message.blade.php
@@ -12,20 +12,18 @@
         Chairman's Message
     @endcomponent
 
-    <div class="container py-3">
-        <div class="row text-justify">
-            <div class="col">
+    <div class="container-fluid">
+
+        <div class="row">
+
+            <div class="col-sm-9 p-4 text-justify">
                 <div class="image-wrapper float-left pr-3">
                     <img class="img-thumbnail img-responsive" src="/img/chairman.jpg" alt="Chairman AFES">
                 </div>
                 <p>While drafting this message, I realized how quickly has 2016 passed and we have just stepped into the New Year 2017. During the past year, we have had remarkable achievements.</p>
                 <p>The focus of our efforts was to establish long lasting relations with local, regional and global evaluation communities during the past 12 months, and we have achieved this milestone very gloriously. AfES became an institutional member to Asia Pacific Evaluation Association (APEA) and Community of Evaluators South Asia (CoE-SA), and have made very good relations with Community of Evaluators Nepal (CoE-N), the IOCE, EvalPartners, and many others among.</p>
-            </div>
-            <div class="col">
                 <p>On the other hand, most importantly, we have made very good reputation in Afghanistan, we are now known by many governmental agencies, ministries and NGOs as a well reputed organization promoting evaluation in the country. Afghan Ministry of Economy requested that we speak and present on best practices on evaluation forums at the 1st National RBME Forum that was held on 20th December, 2016 in Kabul. International representation of AfES was very impressive during the past 12 months; AfES board members attended the APEA Conference in Vietnam, and the EES Conference in the Netherlands.</p>
                 <p>During 2016, AfES launched its series of training titled “The Young and Emerging Evaluators Training Program”, and this series will continue going forward in building the capacity of YEEs in Afghanistan, including through the recent small grant program of IOCE/EvalPartners, in collaboration with Community of Evaluators Nepal.</p>
-            </div>
-            <div class="col">
                 <p>I would like to thank our board members who have dedicated their time, energy and efforts in making all these happen.</p>
                 <p>Lastly, I would like to call on behalf of the board of directors that our mission for 2017 will be more challenging, and the focus of 2017 will be on building individual and institutional capacities in M&E and advancing the theory, practice and use of evaluations; as well as strengthening the institutional capacity of AfES, including increase in our members.</p>
                 <p>Happy New Year 2017 and I hope for a just, equitable and sustainably developed Afghanistan, and the planet. We can improve lives through sound and rigorous evaluations.</p>
@@ -36,6 +34,8 @@
                         Afghan Evaluation Society (AfES)
                 </p>
             </div>
+
+            @include('partials.sectionlist')
         </div>
     </div>
 
