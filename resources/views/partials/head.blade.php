d7bb2d48ce8c17abbed4c222ad856e6a343c8070
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-03-10 17:37:30 +0500

    refactor

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,3 +1,3 @@
-<div class="container py-3" style="color:#733; border-bottom:2px solid #900;">
-    <h1 style="font-family: 'Lato'; font-size:1.8rem;">{{ $slot }}</h1>
+<div class="container-fluid py-3" style="border-bottom:2px solid #900;">
+    <h1 style="color:#733; font-family:'Lato'; font-size:1.8rem;">{{ $slot }}</h1>
 </div>
