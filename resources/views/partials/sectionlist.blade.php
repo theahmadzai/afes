ec8440df7dab2d6a4dff85591eb224b58cacabb3
theahmadzai <theahmadzai@hotmail.com>
2020-03-10 22:51:03 +0500

    section list for about

---
+++ resources/views/partials/sectionlist.blade.php
@@ -0,0 +1,26 @@
+<div class="col-sm-3 p-4 bg-light border-left">
+    <ul style="list-style-type: circle;">
+        <li>
+            <a href="{{ url('/about/chairman-message') }}">Chairman's Message</a>
+        </li>
+        <li>
+            <a href="{{ url('/about/organizational-strategy') }}">Organizational Strategy</a>
+        </li>
+        <li>
+            <a href="{{ url('/about/board-of-directors') }}">Board Of Directors</a>
+        </li>
+        <li>
+            <a href="{{ url('/about/statutes-and-by-laws') }}">Statutes And By-Laws</a>
+            <ul>
+                <a href="{{ url('/about/statutes-and-by-laws/general-provisions') }}">General Provisions</a>
+                <a href="{{ url('/about/statutes-and-by-laws/aim-and-objectives-of-the-organization') }}">Aim and Objectives of the Organization</a>
+                <a href="{{ url('/about/statutes-and-by-laws/governance-structure') }}">Governance Structure</a>
+                <a href="{{ url('/about/statutes-and-by-laws/use-of-property') }}">Use of Property</a>
+                <a href="{{ url('/about/statutes-and-by-laws/staff-recruitment-wages-and-leave') }}">Staff Recruitment Wages and Leave</a>
+                <a href="{{ url('/about/statutes-and-by-laws/reporting-and-financial-operations') }}">Reporting and Financial Operations</a>
+                <a href="{{ url('/about/statutes-and-by-laws/duties-and-responsibilities-of-members') }}">Duties and responsibilities of members</a>
+                <a href="{{ url('/about/statutes-and-by-laws/miscellaneous-decrees') }}">Miscellaneous decrees</a>
+            </ul>
+        </li>
+    </ul>
+</div>
