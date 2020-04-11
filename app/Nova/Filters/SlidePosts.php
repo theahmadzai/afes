89c6d4133b73b8f26fa1c48d4f67a3425166e768
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-04-12 00:47:37 +0500

    new filter nova slideposts

---
+++ app/Nova/Filters/SlidePosts.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Nova\Filters;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Filters\Filter;
+
+class SlidePosts extends Filter
+{
+    /**
+     * The filter's component.
+     *
+     * @var string
+     */
+    public $component = 'select-filter';
+
+    /**
+     * Apply the filter to the given query.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  mixed  $value
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(Request $request, $query, $value)
+    {
+        return $query->where('slide', $value == 'only-slide');
+    }
+
+    /**
+     * Get the filter's available options.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function options(Request $request)
+    {
+        return [
+            'Only Slide Posts' => 'only-slide',
+            'Only No Slide Posts' => 'only-no-slide'
+        ];
+    }
+}
