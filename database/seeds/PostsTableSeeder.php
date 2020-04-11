0201873c2c5c966a5c76bd769457b4081ee6e4b2
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-04-12 00:48:17 +0500

    update post table seeder

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -3,7 +3,8 @@
 use Illuminate\Database\Seeder;
 use App\Post;
 use App\Tag;
-use App\File;
+use App\Image;
+use Illuminate\Support\Collection;
 
 class PostsTableSeeder extends Seeder
 {
@@ -17,9 +18,12 @@ class PostsTableSeeder extends Seeder
         Post::flushEventListeners();
 
         $tags = Tag::all();
+        $imageFileNames = new Collection(['s1.jpg', 's2.jpg', 's3.jpg']);
 
-        factory(Post::class, 20)->create()->each(function ($post) use ($tags) {
-            $post->file()->save(factory(File::class)->make());
+        factory(Post::class, 10)->create()->each(function ($post) use ($tags, $imageFileNames) {
+            $post->image()->save(factory(Image::class)->make([
+                'filename' => $imageFileNames->random(1)->implode(''),
+            ]));
             $post->tags()->attach(
                 $tags->random(rand(1, 3))->pluck('id')->toArray()
             );
