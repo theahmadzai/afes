33de5e1e78b2992f1eee8650ba4784779b99e5b6
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-04-12 00:50:42 +0500

    make carousel get data from posts

--- resources/views/partials/carousel.blade.php
+++ resources/views/partials/carousel.blade.php
@@ -1,26 +1,17 @@
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img class="d-block w-100 h-80 courosel-img-fix" src="/img/s1.jpg" alt="First slide">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Heading One</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab commodi.</p>
+        @foreach ($footer_posts as $post)
+            <div class="carousel-item @if ($loop->first) active @endif">
+                <img class="d-block w-100 h-80 courosel-img-fix" src="{{ $post->imagePath }}" alt="{{ $post->title }}">
+                <div class="carousel-caption d-none d-md-block">
+                    <div class="mb-2 p-2" style="background:#000; opacity:0.6;">
+                        <h5 class="lead">{{ $post->title }}</h5>
+                        <p>{!! Str::words($post->body, 20, '...') !!}</p>
+                    </div>
+                    <a class="btn btn-secondary text-uppercase" href="{{ route('blog', [$post->slug]) }}">READ MORE</a>
+                </div>
             </div>
-        </div>
-        <div class="carousel-item">
-            <img class="d-block w-100 courosel-img-fix" src="/img/s2.jpg" alt="Second slide">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Heading Two</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab commodi.</p>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <img class="d-block w-100 courosel-img-fix" src="/img/s3.jpg" alt="Third slide">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Heading Three</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab commodi.</p>
-            </div>
-        </div>
+        @endforeach
     </div>
     <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
